a9dd16b263e0e1ed7b93bf5e3b654d5ecd9af63f
Demian Velasco <demianvelasco@gmail.com>
2014-05-30 12:56:19 -0400

    updated UI

--- gen/com/demianvelasco/meetingcost/R.java
+++ gen/com/demianvelasco/meetingcost/R.java
@@ -24,13 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080005;
+        public static final int action_settings=0x7f080004;
+        public static final int btn_start_meeting=0x7f080001;
         public static final int editText1=0x7f080003;
-        public static final int editText2=0x7f080004;
-        public static final int textView1=0x7f080002;
+        public static final int input_number_of_employees=0x7f080002;
         public static final int textView2=0x7f080000;
-        public static final int textView3=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,7 +41,20 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int average_salary=0x7f050005;
+        public static final int average_salary_hint=0x7f05000a;
+        public static final int description=0x7f050007;
         public static final int hello_world=0x7f050002;
+        /**  Hints 
+         */
+        public static final int input_number_employee_hint=0x7f050009;
+        /**  Error strings 
+         */
+        public static final int invalid_number_of_employees=0x7f050008;
+        public static final int number_of_employees=0x7f050006;
+        /**  Home Screen Strings 
+         */
+        public static final int start_meeting=0x7f050004;
         public static final int title_activity_timer=0x7f050003;
     }
     public static final class style {
--- src/com/demianvelasco/meetingcost/MainActivity.java
+++ src/com/demianvelasco/meetingcost/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,17 +16,34 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		final Button startMeeting = (Button) findViewById(R.id.button1);
+		final Button startMeeting = (Button) findViewById(R.id.btn_start_meeting);
 		startMeeting.setOnClickListener(new Button.OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				final Intent i = new Intent(MainActivity.this, TimerActivity.class);
+				
+				
+				
+				// Get number of employees and check for valid int
+				final EditText numberOfEmployeesInput = (EditText) findViewById(R.id.input_number_of_employees);
+				final int numberOfEmployees;
+				try{
+					numberOfEmployees = Integer.parseInt(numberOfEmployeesInput.getText().toString());
+				} catch( final NumberFormatException e){
+					Toast.makeText(getApplicationContext(), getResources().getString(R.string.invalid_number_of_employees), Toast.LENGTH_LONG).show();
+					return;
+				}
+				
+				if (numberOfEmployees < 1) {
+					Toast.makeText(getApplicationContext(), getResources().getString(R.string.invalid_number_of_employees), Toast.LENGTH_LONG).show();
+					return;
+				}
+				
+				
+				
 				startActivity(i);
-				
-				
-				
 			}
 			
 			
