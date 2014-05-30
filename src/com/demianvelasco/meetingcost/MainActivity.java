1888f62d83b103cf4101a7a593f8e8751fbf2c01
Demian Velasco <demianvelasco@gmail.com>
2014-05-30 11:32:55 -0400

    Input employees

--- src/com/demianvelasco/meetingcost/MainActivity.java
+++ src/com/demianvelasco/meetingcost/MainActivity.java
@@ -2,7 +2,10 @@ package com.demianvelasco.meetingcost;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button startMeeting = (Button) findViewById(R.id.button1);
+		startMeeting.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				final Intent i = new Intent(MainActivity.this, TimerActivity.class);
+				startActivity(i);
+				
+				
+				
+			}
+			
+			
+		});
+		
 	}
 
 	@Override
