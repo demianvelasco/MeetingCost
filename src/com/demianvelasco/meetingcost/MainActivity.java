8b1d7606b5389e501ecb723312288bf84adc100a
Demian Velasco <demianvelasco@gmail.com>
2014-05-30 03:38:00 -0400

    Replaced activities

--- src/com/demianvelasco/meetingcost/MainActivity.java
+++ src/com/demianvelasco/meetingcost/MainActivity.java
@@ -1,54 +1,15 @@
 package com.demianvelasco.meetingcost;
 
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		
-		final Button startMeeting = (Button) findViewById(R.id.startButton);
-		startMeeting.setOnClickListener(new Button.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				
-				// Start counter
-				
-				
-				// Start button
-				final EditText numberOfEmployeesInput = (EditText) findViewById(R.id.numberOfEmployees);
-				final int numberOfEmployees;
-				
-				try {
-					numberOfEmployees = Integer.parseInt(numberOfEmployeesInput.getText().toString());
-				} catch (final NumberFormatException e) {
-					Toast.makeText(getApplicationContext(), getResources().getString(R.string.invalid_number_of_employees), Toast.LENGTH_LONG).show();
-					return;
-				}
-				
-				Toast.makeText(getApplicationContext(),"Employees: " + numberOfEmployees, Toast.LENGTH_LONG).show();
-				
-			}
-			
-			
-		});
-		
-		
-		
 	}
 
 	@Override
@@ -57,10 +18,5 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	
-	
-
-	
 
 }
